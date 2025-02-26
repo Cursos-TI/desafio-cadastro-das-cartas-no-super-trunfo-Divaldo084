6eaa5ef6c9942ec2dba217f3adb18483a2a53419
Divaldo084 <divaldojunior1029@gmail.com>
2025-02-25 22:33:45 -0300

    Update CartasSuperTrunfo.c DIVALDO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste Divaldo
-
-int main() {
-
-    #include <stdio.h>
-
-    int main(){
+printf("carta 1:\n");
 
         char estado;
         char codigo[50];
@@ -21,10 +10,10 @@ int main() {
         int numero_de_pontos_turisticos;
 
 
-        printf("Digite o nome do estado:\n");
+        printf("Digite o nome do estado:(ex: A a H)\n");
         scanf("%s", &estado);
 
-        printf("Digite o código do estado:\n");
+        printf("Digite o código do estado:(EX: a01, b03)\n");
         scanf("%s", &codigo);
 
         printf("Digite o nome da cidade:\n");
@@ -41,6 +30,47 @@ int main() {
 
         printf("digite o numero de pontos turisticos:\n");
         scanf("%d", &numero_de_pontos_turisticos);
+
+            
+    
+
+        printf("carta 2:\n");
+
+              
+        printf("Digite o nome do estado:(ex: A a H)\n");
+        scanf("%s", &estado);
+
+        printf("Digite o código do estado:(EX: a01, b03)\n");
+        scanf("%s", &codigo);
+
+        printf("Digite o nome da cidade:\n");
+        scanf("%s", &nome_da_cidade);   
+
+        printf("Digite a população:\n");
+        scanf("%d", &população);
+
+        printf("digite a area:\n");
+        scanf("%f", &area);
+
+        printf("digite o pib:\n");
+        scanf("%f", &pib);
+
+        printf("digite o numero de pontos turisticos:\n");
+        scanf("%d", &numero_de_pontos_turisticos);
+
+        return 0;
+    }
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das Cartas
+// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
+// Siga os comentários para implementar cada parte do desafio.
+//Teste Divaldo
+
+int main() {
+
+    #include <stdio.h>
+
+    int main(){
 
         
         // Sugestão: Defina variáveis separadas para cada atributo da cidade.
